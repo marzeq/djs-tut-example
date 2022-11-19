65cb3de5d0898cca739d4d0c9381fb947e79bd73
marzeq <marzeqmarzeq@gmail.com>
2022-11-19 22:04:35 +0100

    step 9: add command code

--- src/modules/example.ts
+++ src/modules/example.ts
@@ -1,5 +1,11 @@
 import { BotType } from "../index"
 
 export const module = async (client: BotType) => {
-  console.log("Example module loaded")
+  client.commands.set("ping", {
+    name: "ping",
+    description: "Pong!",
+    run: async interaction => {
+      await interaction.reply("Pong!")
+    },
+  })
 }
