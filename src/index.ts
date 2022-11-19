fa4b5a8060d63919bd55e5136597bd2529f618ed
marzeq <marzeqmarzeq@gmail.com>
2022-11-19 21:53:02 +0100

    step 5: add module support

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,30 @@ export interface Command extends ChatInputApplicationCommandData {
   run: (interaction: ChatInputCommandInteraction) => Awaitable<any>
 }
 
+export type Module = Promise<{
+  default: (client: Bot) => Awaitable<any>
+} | {
+  module: (client: Bot) => Awaitable<any>
+}>
+
+const loadModules = async (modules: Module[], client: Bot) => {
+  for (const module of await Promise.all(modules)) {
+    if ("default" in module) {
+      module.default(client)
+    } else {
+      module.module(client)
+    }
+  }
+}
+
 const main = async () => {
   const client = new Bot({
     intents: ["Guilds"]
   })
+
+  await loadModules([
+    import("./modules/example"),
+  ], client)
 
   if (!("TOKEN" in process.env)) {
     throw new Error("No token provided")
